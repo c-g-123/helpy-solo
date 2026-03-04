85afb449046c3c8c7ab7bd84faaec58ed2256918
Kunter Cetin <3075044C@student.gla.ac.uk>
2026-03-04 17:35:56 +0000

    feat: load user projects on create task page

--- core/views/task.py
+++ core/views/task.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from core.models import Project
 
 @login_required
 def create_task(request):
-    return render(request, 'core/create_task.html')
+    # Get only the projects that belong to the user, so that they can only assign tasks to their own projects.
+    projects = Project.objects.filter(user_id=request.user)
+    return render(request, 'core/create_task.html', {'projects': projects})
 
 
 @login_required
