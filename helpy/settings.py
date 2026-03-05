c8872df950b7358090d9b921eb6ec843fc280137
Imogen Graham <imogenmcgraham@gmail.com>
2026-03-05 13:16:12 +0000

    static files (should) now work, and files have been added for all pages

--- helpy/settings.py
+++ helpy/settings.py
@@ -17,6 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production.
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -36,6 +37,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = 'core:login'
+
+STATICFILES_DIRS = [STATIC_DIR, ]
+
 
 # =====================================================================================================
 
@@ -129,4 +133,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
