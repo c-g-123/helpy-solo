728fd1eac4103023822e5bab63ffc983b995cb2b
Kunter Cetin <3075044C@student.gla.ac.uk>
2026-03-07 22:00:44 +0000

    feat: handle settings and email forms in account view

--- core/views/other.py
+++ core/views/other.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from core.models import UserSettings
-from core.forms import UserSettingsForm
+from core.forms import UserSettingsForm, UserEmailForm
 
 def index(request):
     return render(request, 'core/index.html')
@@ -12,16 +12,27 @@ def index(request):
 def account(request):
     user_settings, created = UserSettings.objects.get_or_create(user=request.user)
 
-    if request.method == 'POST':
-        form = UserSettingsForm(request.POST, instance=user_settings)
+    settings_form = UserSettingsForm(instance=user_settings)
+    email_form = UserEmailForm(instance=request.user)
 
-        if form.is_valid():
-            form.save()
-    else:
-        form = UserSettingsForm(instance=user_settings)
+    if request.method == 'POST':
+        form_type = request.POST.get('form_type')
+
+        if form_type == 'settings':
+            settings_form = UserSettingsForm(request.POST, instance=user_settings)
+
+            if settings_form.is_valid():
+                settings_form.save()
+        
+        elif form_type == 'email':
+            email_form = UserEmailForm(request.POST, instance=request.user)
+
+            if email_form.is_valid():
+                email_form.save()
     
     context = {
-        'form': form,
+        'settings_form': settings_form,
+        'email_form': email_form,
     }
 
     return render(request, 'core/settings.html', context)
