6296b05fc1399e34357b8795c0ec8ff7d78ee5f2
c-g <no-email>
2026-03-09 15:27:18 +0000

    refactor: move misc views into core.views.__init__.py

--- core/views/authorisation.py
+++ core/views/authorisation.py
@@ -9,7 +9,7 @@ from .utils import render_error_message
 
 def register(request):
     if request.method != 'POST':
-        return render(request, 'core/auth/register.html')
+        return render(request, 'core/authorisation/register.html')
 
     username = request.POST.get('username')
     password = request.POST.get('password')
@@ -18,11 +18,11 @@ def register(request):
     # Validation
 
     if not username:
-        return render_error_message(request, template='core/auth/register.html', error_message='Username cannot be empty.')
+        return render_error_message(request, template='core/authorisation/register.html', error_message='Username cannot be empty.')
     if not password:
-        return render_error_message(request, template='core/auth/register.html', error_message='Password cannot be empty.')
+        return render_error_message(request, template='core/authorisation/register.html', error_message='Password cannot be empty.')
     if password != repeat_password:
-        return render_error_message(request, template='core/auth/register.html', error_message='Passwords do not match.')
+        return render_error_message(request, template='core/authorisation/register.html', error_message='Passwords do not match.')
 
     user = User.objects.create_user(username=username, password=password)
 
@@ -36,7 +36,7 @@ def login(request):
         if request.user.is_authenticated:
             return redirect(reverse('core:calendar'))  # WARNING Change this to redirect to the default dashboard chosen by the user.
 
-        return render(request, 'core/auth/login.html')
+        return render(request, 'core/authorisation/login.html')
 
     username = request.POST.get('username')
     password = request.POST.get('password')
@@ -44,9 +44,9 @@ def login(request):
     user = auth.authenticate(username=username, password=password)
 
     if not user:
-        return render_error_message(request, template='core/auth/login.html', error_message='Invalid username or password.')
+        return render_error_message(request, template='core/authorisation/login.html', error_message='Invalid username or password.')
     if not user.is_active:
-        return render_error_message(request, template='core/auth/login.html', error_message='Your account is disabled.')
+        return render_error_message(request, template='core/authorisation/login.html', error_message='Your account is disabled.')
 
     auth.login(request, user)
     return redirect(reverse('core:calendar'))  # WARNING Change this to redirect to the default dashboard chosen by the user.
